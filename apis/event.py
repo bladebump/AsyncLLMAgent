1e353a8a760d69c66904b92cfd0c6b65d6343623
bladebump <yfa6190059@hotmail.com>
2025-04-07 10:14:23 +0800

    更新事件分析API，重构Event模型以支持会话ID、时间戳和base64编码数据；新增EventPost模型以处理事件列表，优化事件内容解析逻辑。

--- apis/event.py
+++ apis/event.py
@@ -3,30 +3,45 @@ from pydantic import BaseModel
 import json
 from core.llms import AsyncBaseChatCOTModel
 from .utils import get_llm, get_llm_cot, parse_markdown_json
+import base64
 
 event_router = APIRouter(prefix="/event")
 
 class Event(BaseModel):
-    event_content: str
+    session_id: str
+    timestamp: int
+    data: str # base64编码的内容
+
+class EventPost(BaseModel):
+    event_list: list[Event]
     use_cot_model: bool = False
 
 @event_router.post("/event_analysis")
-async def event_analysis(event: Event, llm:AsyncBaseChatCOTModel = Depends(get_llm),cot_llm:AsyncBaseChatCOTModel = Depends(get_llm_cot)):
+async def event_analysis(event: EventPost, llm:AsyncBaseChatCOTModel = Depends(get_llm),cot_llm:AsyncBaseChatCOTModel = Depends(get_llm_cot)):
+    event_list = []
+    for event in event.event_list:
+        event_list.append({
+            "session_id": event.session_id,
+            "timestamp": event.timestamp,
+            "data": base64.b64decode(event.data).decode("utf-8")
+        })
     prompt = f"""
 # 事件内容
-{event.event_content}
+{event_list}
 
 # 指令
 以上是捕获到的tty log，请分析出用户做了什么操作,输出格式如下，使用json格式
 
 # 输出格式
 [{{
-    "event_input": "用户操作",
+    "seesion_id": "会话id",
     "event_name": "用户做了什么操作",
+    "event_input": "用户操作",
     "event_output": "用户操作的输出，输出比较短的时候，给出完整输出，输出比较长的时候，给出输出总结",
     "event_info": "操作的详细信息",
-    "event_time": "操作的时间",
-    "event_special_info": "操作的特殊信息"
+    "event_special": "操作的特殊信息",
+    "event_start": "操作开始的时间戳",
+    "event_end": "操作结束的时间戳"
 }}]
 """
     use_llm = cot_llm if event.use_cot_model else llm
